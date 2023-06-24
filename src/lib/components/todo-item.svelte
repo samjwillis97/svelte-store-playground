7fd13734ca66da475c88b92a629fb84d4de04539
Sam Willis <sam@williscloud.org>
2023-06-24 15:54:26 +1000

    More prettying up :)

--- src/lib/components/todo-item.svelte
+++ src/lib/components/todo-item.svelte
@@ -2,18 +2,11 @@
 	import { failure, success } from '$lib/my-toast';
 	import type { TodoItem } from '$lib/service';
 	import { deleteTodo } from '$lib/store';
-	import {
-		Dialog,
-		DialogContent,
-		DialogDescription,
-		DialogFooter,
-		DialogHeader,
-		DialogTitle,
-		DialogTrigger
-	} from './ui/dialog';
+	import { Trash2 } from 'lucide-svelte';
 	import { Label } from './ui/label';
-	import { Button, buttonVariants } from './ui/button';
-	import { Input } from './ui/input';
+	import { Button } from './ui/button';
+	import { Card } from './ui/card';
+	import TodoPopup from './todo-popup.svelte';
 
 	export let todo: TodoItem;
 
@@ -29,51 +22,17 @@
 
 {#if todo}
 	<!-- TODO: if the element itself is still loading disable buttons (don't know how, like how to know whether it is real)-->
-	<div
-		class="w-96 border shadow rounded px-5 py-5 flex flex-row gap-2.5 justify-between items-center"
-	>
+	<Card class="w-96 flex flex-row gap-2.5 justify-between items-center px-5 py-5">
 		<div class="flex flex-row gap-2.5">
-			<p class="text-xl">
+			<Label>
 				{todo.name}
-			</p>
+			</Label>
 		</div>
 		<div class="flex flex-row gap-2.5">
-			<button
-				disabled={true}
-				class="py-2 px-4 rounded text-white bg-blue-500 hover:bg-blue-700 disabled:bg-blue-200"
-			>
-				Info
-			</button>
-			<button
-				on:click={() => $deleteMutator.mutate(todo.name)}
-				class="py-2 px-4 rounded text-white bg-red-500 hover:bg-red-700 active:bg-red-800 disabled:bg-red-200"
-			>
-				Delete
-			</button>
-			<Dialog modal={true}>
-				<DialogTrigger class={buttonVariants({ variant: 'outline' })}>Edit Profile</DialogTrigger>
-				<DialogContent class="sm:max-w-[425px]">
-					<DialogHeader>
-						<DialogTitle>Edit profile</DialogTitle>
-						<DialogDescription>
-							Make changes to your profile here. Click save when you're done.
-						</DialogDescription>
-					</DialogHeader>
-					<div class="grid gap-4 py-4">
-						<div class="grid grid-cols-4 items-center gap-4">
-							<Label class="text-right">Name</Label>
-							<Input id="name" value="Pedro Duarte" class="col-span-3" />
-						</div>
-						<div class="grid grid-cols-4 items-center gap-4">
-							<Label class="text-right">Username</Label>
-							<Input id="username" value="@peduarte" class="col-span-3" />
-						</div>
-					</div>
-					<DialogFooter>
-						<Button type="submit">Save changes</Button>
-					</DialogFooter>
-				</DialogContent>
-			</Dialog>
+			<TodoPopup {todo} />
+			<Button variant="destructive" on:click={() => $deleteMutator.mutate(todo.name)}>
+				<Trash2 size="18" />
+			</Button>
 		</div>
-	</div>
+	</Card>
 {/if}
---
+++ src/lib/components/todo-popup.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import {
+		Dialog,
+		DialogContent,
+		DialogDescription,
+		DialogFooter,
+		DialogHeader,
+		DialogTitle,
+		DialogTrigger
+	} from './ui/dialog';
+	import { Label } from './ui/label';
+	import { Button, buttonVariants } from './ui/button';
+	import { Input } from './ui/input';
+	import type { TodoItem } from '$lib/service';
+	import { Textarea } from './ui/textarea';
+
+	export let todo: TodoItem;
+</script>
+
+<Dialog modal={true}>
+	<DialogTrigger class={buttonVariants({ variant: 'default' })}>Open</DialogTrigger>
+	<DialogContent class="sm:max-w-[425px]">
+		<DialogHeader>
+			<DialogTitle>{todo.name}</DialogTitle>
+			<DialogDescription>
+				Make changes to the item here. Click save when you're done.
+			</DialogDescription>
+		</DialogHeader>
+		<Textarea placeholder="Insert summary here..." />
+		<DialogFooter>
+			<Button type="submit">Save changes</Button>
+		</DialogFooter>
+	</DialogContent>
+</Dialog>
